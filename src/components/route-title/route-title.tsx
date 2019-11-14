1a949869d543dd25b9c82ccfd650f8299154a443
Lukas <theluksys3@gmail.com>
2019-11-14 19:09:24 +0100

    chore: restructured project

---
+++ src/components/route-title/route-title.tsx
@@ -0,0 +1,31 @@
+import { Component, Prop, Element, Watch, ComponentInterface } from '@stencil/core';
+import ActiveRouter from '../../global/active-router';
+
+/**
+ * Updates the document title when found.
+ *
+ * @name RouteTitle
+ * @description
+ */
+@Component({
+	tag: 'stencil-route-title'
+})
+export class RouteTitle implements ComponentInterface {
+	@Element() el!: HTMLElement;
+	@Prop() titleSuffix: string = '';
+	@Prop() pageTitle: string = '';
+
+	@Watch('pageTitle')
+	updateDocumentTitle() {
+		const el = this.el;
+		if (el.ownerDocument) {
+			el.ownerDocument.title = `${this.pageTitle}${this.titleSuffix || ''}`;
+		}
+	}
+
+	componentWillLoad() {
+		this.updateDocumentTitle();
+	}
+}
+
+ActiveRouter.injectProps(RouteTitle, ['titleSuffix']);
